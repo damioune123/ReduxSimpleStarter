37ebdc2ae1b8efb0b17fa78880bc8a8c40363b18
Damien Meur <damienmeur@gmail.com>
2017-10-24 00:56:17 +0200

    lessson 63 : props to a render helper

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const API_KEY = e606ca90ad8d97d3a12a831f6ede1651;
+const API_KEY = "e606ca90ad8d97d3a12a831f6ede1651";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_kEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
-export function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},BE`;
-    const request = axios.get(url);
 
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},be`;
+    const request = axios.get(url);
     return{
         type: FETCH_WEATHER,
         payload: request
-
     }
     
 }
